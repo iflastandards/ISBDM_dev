b50207898048bafe9a45dd3309f65317a79e1313
Jon Phipps <jphipps@madcreek.com>
2025-03-26 20:48:25 -0400

    update config

--- docs/docusaurus.config.ts
+++ docs/docusaurus.config.ts
@@ -12,7 +12,7 @@ const config: Config = {
   // Set the production url of your site here
   url: 'https://iflastandards.github.io', // GitHub organization page  // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
+  baseUrl: '/ISBDM_dev/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
